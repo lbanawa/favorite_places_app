93cc44545fa1301343f6f48fdf5add1b413335b6
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-05-19 19:36:06 -0400

    addLocationVC

---
+++ FavoritePlacesApp/AddLocationVC.swift
@@ -0,0 +1,27 @@
+//
+//  AddLocationVC.swift
+//  FavoritePlacesApp
+//
+//  Created by Lauren Banawa on 5/19/20.
+//  Copyright © 2020 Lauren Banawa. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationVC: UIViewController {
+    @IBOutlet weak var locationNameText: UITextField!
+    @IBOutlet weak var locationTypeText: UITextField!
+    @IBOutlet weak var locationDescriptionText: UITextView!
+    @IBOutlet weak var locationImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        
+    }
+    
+}
