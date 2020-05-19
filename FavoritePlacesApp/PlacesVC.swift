73bea1de45d8d9aec57b1b4a77c77b3d7a86fd8c
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-05-19 18:53:23 -0400

    auto login feature and logout button

--- FavoritePlacesApp/PlacesVC.swift
+++ FavoritePlacesApp/PlacesVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -16,10 +17,27 @@ class PlacesVC: UIViewController {
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "L O G O U T", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
+        
     }
     
-    @objc func addButtonClicked(){
+    @objc func addButtonClicked() {
         // segue to add places view controller
+    }
+    
+    @objc func logoutButtonClicked() {
+        // logout with block so that if you lose internet connection you can show the error to the user
+        PFUser.logOutInBackground { (error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+                
+            } else {
+                self.performSegue(withIdentifier: "toSignInVC", sender: nil)
+            }
+        }
     }
     
 
