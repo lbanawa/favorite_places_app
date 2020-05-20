9c3313894f346c938058f70096be9071fd98d299
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-05-20 17:52:13 -0400

    retrieve data from Parse database

--- FavoritePlacesApp/PlacesVC.swift
+++ FavoritePlacesApp/PlacesVC.swift
@@ -9,8 +9,12 @@
 import UIKit
 import Parse
 
-class PlacesVC: UIViewController {
+class PlacesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     @IBOutlet weak var tableView: UITableView!
+    
+    var locationNameArray = [String]()
+    var locationIdArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,43 @@ class PlacesVC: UIViewController {
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "L O G O U T", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        getDataFromParse()
+        
+    }
+    
+    func getDataFromParse () {
+        
+        let query = PFQuery(className: "Places")
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+            } else {
+                if objects != nil {
+                    
+                    self.locationIdArray.removeAll(keepingCapacity: false)
+                    self.locationNameArray.removeAll(keepingCapacity: false)
+                    
+                    for object in objects! {
+                        if let locationName = object.object(forKey: "name") as? String {
+                            if let locationId = object.objectId {
+                                self.locationNameArray.append(locationName)
+                                self.locationIdArray.append(locationId)
+                            }
+                        }
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+                
+            }
+        }
+    
         
     }
     
@@ -29,15 +70,30 @@ class PlacesVC: UIViewController {
         // logout with block so that if you lose internet connection you can show the error to the user
         PFUser.logOutInBackground { (error) in
             if error != nil {
-                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
-                alert.addAction(okButton)
-                self.present(alert, animated: true, completion: nil)
-                
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
             } else {
                 self.performSegue(withIdentifier: "toSignInVC", sender: nil)
             }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationNameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = locationNameArray[indexPath.row]
+        return cell
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+
     }
     
 
